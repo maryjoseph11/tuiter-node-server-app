1fcdbbd5c8de45f0bec47cd4f83413910ed68687
maryjoseph11 <georgerhea06@gmail.com>
2023-04-14 01:04:07 -0400

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-
+import mongoose from "mongoose";
+mongoose.connect('mongodb+srv://josephmary1:supersecretpassword@cluster0.r5ik6p2.mongodb.net/?retryWrites=true&w=majority');
 const app = express();
 app.use(cors())
 app.use(express.json());
